902a8d3fc32c5c173b813839f195e85dffcb416f
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-07-03 01:42:14 +0530

    add: nix cleanup automation

--- darwin.nix
+++ darwin.nix
@@ -143,5 +143,11 @@
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.gc = {
+    automatic = true;
+    interval = { Weekday = 0; Hour = 3; Minute = 0; };
+    options = "--delete-older-than 30d";
+  };
+  nix.optimise.automatic = true;
   system.stateVersion = 5;
 }

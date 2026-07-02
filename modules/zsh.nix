0d0668a89792007a7aa2220534d8390317ca9431
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-07-03 00:06:52 +0530

    history addition

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -6,6 +6,14 @@
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
+    history = {
+      path = "$HOME/.zsh_history";
+      size = 50000;
+      save = 50000;
+      ignoreDups = true;
+      ignoreSpace = true;
+      shareHistory = true; 
+    };
     initContent = ''
       eval "$(/opt/homebrew/bin/brew shellenv)"
     '';

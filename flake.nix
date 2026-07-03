963805186444b6399f09db4c550c1ef6d570269e
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-07-03 13:07:52 +0530

    updated github link for nix-darwin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    darwin.url = "github:LnL7/nix-darwin";
+    darwin.url = "github:nix-darwin/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     

b81e678032a9e3ff0ba85a2d039f519869afbc5e
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-07-05 17:13:48 +0530

    beautify nvim

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -5,22 +5,29 @@
     enable = true;
     defaultEditor = true;
 
+    plugins = with pkgs.vimPlugins; [
+      rose-pine
+      nvim-web-devicons               # Needed for Lualine icons
+      lualine-nvim                    # The status bar
+      nvim-treesitter.withAllGrammars # Rich syntax highlighting
+    ];
+
     initLua = ''
+      -- ==========================================
+      -- Core Options
+      -- ==========================================
       vim.opt.number = true
       vim.opt.relativenumber = true
       vim.opt.termguicolors = true
-
       vim.opt.tabstop = 4
       vim.opt.shiftwidth = 4
       vim.opt.expandtab = true
       vim.opt.smartindent = true
       vim.opt.autoindent = true
-
       vim.opt.ignorecase = true
       vim.opt.smartcase = true
       vim.opt.hlsearch = false
       vim.opt.incsearch = true
-
       vim.opt.mouse = 'a'
       vim.opt.clipboard = 'unnamedplus'
       vim.opt.scrolloff = 8
@@ -31,9 +38,18 @@
       vim.opt.signcolumn = 'yes'
       vim.opt.cursorline = true
       vim.opt.showmode = false
-
       vim.opt.undofile = true
       vim.opt.undodir = vim.fn.stdpath('data') .. '/undo'
+
+      -- ==========================================
+      -- Theme Setup
+      -- ==========================================
+      require("rose-pine").setup({
+          styles = {
+              transparency = true,
+          }
+      })
+      vim.cmd("colorscheme rose-pine")
 
       local clears = {
         "Normal", "NormalNC", "NormalFloat", "FloatBorder",
@@ -45,6 +61,35 @@
       vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
       vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#524f67" })
 
+      -- ==========================================
+      -- Safe Plugin Loading (No Crashes)
+      -- ==========================================
+      
+      -- Treesitter (Syntax Highlighting)
+      local ts_ok, ts = pcall(require, "nvim-treesitter.configs")
+      if ts_ok then
+        ts.setup({
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+      end
+
+      -- Lualine (Status Bar)
+      local lualine_ok, lualine = pcall(require, "lualine")
+      if lualine_ok then
+        lualine.setup({
+          options = {
+            theme = 'rose-pine',
+            component_separators = '|',
+            section_separators = { left = '', right = '' },
+            globalstatus = true,
+          }
+        })
+      end
+
+      -- ==========================================
+      -- Keymaps
+      -- ==========================================
       vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = "Save" })
       vim.keymap.set('n', '<leader>q', ':q<CR>', { desc = "Quit" })
       vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')

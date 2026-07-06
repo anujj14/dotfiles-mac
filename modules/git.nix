33eadf28741b5f7486c27d68aed90c4faf84b6b6
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-07-06 10:45:34 +0530

    signing key inside user block

--- modules/git.nix
+++ modules/git.nix
@@ -7,10 +7,10 @@
       user = {
         name = "Anuj Pokhriyal";
         email = "77380156+anujj14@users.noreply.github.com";
+        signingkey = "~/.ssh/id_ed25519_signing.pub"; 
       };
       commit.gpgsign = true;
       gpg.format = "ssh";
-      user.signingkey = "~/.ssh/id_ed25519_signing.pub";
     };
     ignores = [
       ".DS_Store"

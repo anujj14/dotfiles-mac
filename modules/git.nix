f545d6e8e0e67cfef3ec61f0658206169aa96fc7
Anuj Pokhriyal <77380156+anujj14@users.noreply.github.com>
2026-09-03 17:59:31 +0530

    git: update ssh signing key to id_ed25519_github

--- modules/git.nix
+++ modules/git.nix
@@ -7,7 +7,7 @@
       user = {
         name = "Anuj Pokhriyal";
         email = "77380156+anujj14@users.noreply.github.com";
-        signingkey = "~/.ssh/id_ed25519_signing.pub"; 
+        signingkey = "~/.ssh/id_ed25519_github.pub"; 
       };
       commit.gpgsign = true;
       gpg.format = "ssh";
